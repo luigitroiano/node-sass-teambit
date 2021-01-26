22cb9adde187be6e27183e29953761350d0f9d87
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-26 20:12:54 +0100

    Header Completato

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,25 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: "#root",
+  data: {
+    menuNavbar: [{
+      name: "Solutions",
+      url: "#"
+    }, {
+      name: "Pricing",
+      url: "#"
+    }, {
+      name: "Blog",
+      url: "#"
+    }, {
+      name: "Sign in",
+      url: "#"
+    }]
+  },
+  methods: {}
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,26 @@
+let app = new Vue({
+  el: "#root",
+  data: {
+    menuNavbar: [
+      {
+        name: "Solutions",
+        url: "#",
+      },
+      {
+        name: "Pricing",
+        url: "#",
+      },
+      {
+        name: "Blog",
+        url: "#",
+      },
+      {
+        name: "Sign in",
+        url: "#",
+      },
+    ],  
+  },
+  methods: {
+
+  },
+});
